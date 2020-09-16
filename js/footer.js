bf5a069f86e91a63bb1aa59512d0ec4891a46f34
Sudsier <17775593312@163.com>
2020-09-16 20:55:53 +0800

    Add files via upload

--- js/footer.js
+++ js/footer.js
@@ -2,9 +2,9 @@ document.write("<div>");
 document.write("    <div class=\"footer_1\">");
 document.write("      <div style=\"font-size:30px;color:#fafafa\">Sudsier<\/div>");
 document.write("      <hr style=\"background-color:#fafafa;height:1px;border:none;\">");
-document.write("      <a rel=\"license\" href=\"http:\/\/creativecommons.org\/licenses\/by-nc-nd\/4.0\/\"><img alt=\"知识共享许可协议\" style=\"border-width:0\" src=\"https:\/\/licensebuttons.net\/l\/by-nc-nd\/4.0\/80x15.png\" \/><\/a>除非另有声明，本网站内容采用<span rel=\"license\" href=\"http:\/\/creativecommons.org\/licenses\/by-nc-nd\/4.0\/\" style=\"color:#F02000;\">CC BY-NC-ND 4.0 国际许可协议<\/span>授权。");
+document.write("      <a rel=\"license\" href=\"http:\/\/creativecommons.org\/licenses\/by-nc-nd\/4.0\/\"><img alt=\"知识共享许可协议\" style=\"border-width:0\" src=\"https:\/\/licensebuttons.net\/l\/by-nc-nd\/4.0\/80x15.png\" \/><\/a>除非另有声明，本网站内容采用<span rel=\"license\" href=\"http:\/\/creativecommons.org\/licenses\/by-nc-nd\/4.0\/\" style=\"color:#aaaaaa;\">CC BY-NC-ND 4.0 国际许可协议<\/span>授权。");
 document.write("    <\/div>");
-document.write("    <div class=\"footer_2\">Copyright © 2020-<span id=\"copyright\"><\/span> Sudsier All rights reserved.<br>Sudsier信息组负责维护本网站<\/div>");
+document.write("    <div class=\"footer_2\">Copyright © 2020-<span id=\"copyright\"><\/span> Sudsier All rights reserved.<\/div>");
 document.write("<\/div>");
 //版权年份
 var d = new Date();
--- js/navigation.js
+++ js/navigation.js
@@ -7,22 +7,19 @@ document.write("      <a href=\"\/doc\">文档<\/a>");
 document.write("      <a href=\"\/music\">音乐<\/a>");
 document.write("      <a href=\"#\">关于<\/a>");
 document.write("    <\/div>");
-document.write("    <div style=\"background-color:#FF9180;height:64px;\">");
-document.write("      <span style=\"font-size:30px;color:#4CAF50;position:relative;left:18px;top:11px;cursor:pointer\" onclick=\"openNav()\">&#9776;<\/span>");
+document.write("    <div style=\"background-color:#3ABAFF;height:64px;\">");
+document.write("      <span style=\"font-size:30px;color:#FF7F3A;position:relative;left:18px;top:11px;cursor:pointer\" onclick=\"openNav()\">&#9776;<\/span>");
 document.write("      <a href=\"\/\"><img src=\"https:\/\/sudsier.gitee.io\/webimg\/icon.png\" width=\"200px\" height=\"44p\" style=\"position:absolute;left:92px;top:10px\"><\/a>");
 document.write("    <\/div>");
 document.write("  <\/div>");
 document.write("  <div class=\"id_name\">");
-document.write("    <div style=\"background-color:#FF9180;text-align:center\">");
-document.write("    <img src=\"https:\/\/s1.ax1x.com\/2020\/06\/25\/NDiHYD.md.png\" width=\"200px\" height=\"44px\"><\/div>");
 document.write("    <div class=\"topnav\">");
-document.write("      <a class=\"active\" href=\"\/\">首页<\/a>");
+document.write("      <img src=\"https:\/\/s1.ax1x.com\/2020\/06\/25\/NDiHYD.md.png\" width=\"160px\" style=\"float: left;padding: 7px 14px 0 14px;\"><a class=\"active\" href=\"\/\">首页<\/a>");
 document.write("      <a href=\"\/doc\">文档<\/a>");
 document.write("      <a href=\"\/music\">音乐<\/a>");
 document.write("      <a href=\"#about\">关于<\/a>");
 document.write("    <\/div>");
 document.write("<\/div>");
-document.write("<div style=\"text-align: center;margin-top: 15px\"><img src=\"\/\/sudsier.gitee.io\/webimg\/20200825.png\" width=\"300px\"><\/div>");
 /* 将侧导航的宽度设置为250px，页面内容的左边距设置为250px，并在正文中添加黑色背景色 */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
